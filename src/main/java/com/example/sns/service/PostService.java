36b9cfd7a2539c4edd407681b37b2541f1c3c952
nakgyeom <himina1227@gmail.com>
2023-07-16 00:02:13 +0900

    - Post dto 생성 및 수정 삭제 비지니스 로직 추가

--- src/main/java/com/example/sns/service/PostService.java
+++ src/main/java/com/example/sns/service/PostService.java
@@ -2,6 +2,7 @@ package com.example.sns.service;
 
 import com.example.sns.exception.ErrorCode;
 import com.example.sns.exception.SnsApplicationException;
+import com.example.sns.model.Post;
 import com.example.sns.model.entity.PostEntity;
 import com.example.sns.model.entity.UserEntity;
 import com.example.sns.repository.PostEntityRepository;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Slf4j
 @Transactional
@@ -27,4 +30,25 @@ public class PostService {
         PostEntity postEntity = repository.save(PostEntity.of(title, body, userEntity));
         repository.save(postEntity);
     }
+
+    public Post modify(Integer userId, Integer postId, String title, String body) {
+        PostEntity postEntity = repository.findById(postId).orElseThrow(() -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("postId is %d", postId)));
+        if (!Objects.equals(postEntity.getUser().getId(), userId)) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("user %s has no permission with post %d", userId, postId));
+        }
+
+        postEntity.setTitle(title);
+        postEntity.setBody(body);
+
+        return Post.fromEntity(repository.saveAndFlush(postEntity));
+    }
+
+    public void delete(Integer userId, Integer postId) {
+        PostEntity postEntity = repository.findById(postId).orElseThrow(() -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("postId is %d", postId)));
+        if (!Objects.equals(postEntity.getUser().getId(), userId)) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("user %s has no permission with post %d", userId, postId));
+        }
+        repository.delete(postEntity);
+    }
+
 }
--- src/test/java/com/example/sns/fixture/UserEntityFixture.java
+++ src/test/java/com/example/sns/fixture/UserEntityFixture.java
@@ -1,6 +1,10 @@
 package com.example.sns.fixture;
 
+import com.example.sns.model.UserRole;
 import com.example.sns.model.entity.UserEntity;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 public class UserEntityFixture {
 
@@ -9,6 +13,8 @@ public class UserEntityFixture {
         entity.setId(1);
         entity.setUserName(userName);
         entity.setPassword(password);
+        entity.setRole(UserRole.USER);
+        entity.setRegisteredAt(Timestamp.from(Instant.now()));
         return entity;
     }
 }
